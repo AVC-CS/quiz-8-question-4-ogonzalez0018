884b1bf3fa48e7042400b5a35d8ac5ba0ebfafec
ogonzalez0018 <156852162+ogonzalez0018@users.noreply.github.com>
2024-02-23 19:49:55 +0000

    1200pm

--- main.hpp
+++ main.hpp
@@ -11,5 +11,17 @@ int splitline(string, char, char[][MAXLEN]);
 int program_main(){
     string text = "ABCDEF,GHI,JKL,MNO";
     char target[MAXNUM][MAXLEN];
-    
+
+    int cnt = splitline(txt, ',', target);
+
+    cout << "Number  of split words: " << cnt << endl;
+    for (int i = 0; i < cnt; ++i) {
+        cout << "Word" << i + 1 << ": " << target[i] << endl;
+    }
+
+    return 0;
+
+}
+
+
 }
